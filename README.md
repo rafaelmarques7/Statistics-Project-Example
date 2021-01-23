55c23e8520c5753793af4a258c2f60adadbed210
rafael <rafael@BMDIG0021.local>
2021-01-23 16:42:50 +0000

    write the proposed project using javascript

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ The program should:
 * read a *student-grades.txt* file from a directory
 * calculate the *average*, the *standard deviation*, and the *mode* of the grades
 
+The *student-grades.txt* files is organized so that:
+* each line contains the student name, followed by the student grade; the data is separated by a comma
+
---
+++ javascript/index.js
@@ -0,0 +1,76 @@
+const fs = require('fs')
+
+fpStudentGrades = '../student-grades.txt'
+
+// read the file and its contents
+const file = fs.readFileSync(fpStudentGrades)
+const content = file.toString()
+
+// parse the contents into an array - each line an array element
+const contentArray = content.split('\n')
+
+// organize the contents into a structured object
+const contentObject = {}
+contentArray.forEach(contentLine => {
+  const dataLine = contentLine.split(',')
+  const [studentName, studentGrade] = dataLine
+  contentObject[studentName] = Number(studentGrade)
+});
+
+function calculateAverage(arrayNumbers) {
+  let sum = 0
+  arrayNumbers.forEach(value => {
+    sum += value
+  })
+
+  const average = sum/arrayNumbers.length
+  return average
+}
+
+const arrayGrades = Object.keys(contentObject).map(studentName => {
+  return contentObject[studentName]
+})
+
+const average = calculateAverage(arrayGrades)
+console.log('average: ', average)
+
+function calculateMode(arrayNumbers) {
+  const sortedArrayNumbers = arrayNumbers.sort()
+  const numElements = sortedArrayNumbers.length
+  const hasEvenNumberOfElements = numElements % 2 === 0
+
+  if (hasEvenNumberOfElements) {
+    // calculate the value of the middle two elements
+    const indexMiddleLeft = numElements / 2 - 1
+    const indexMiddleRight = numElements / 2
+    return (sortedArrayNumbers[indexMiddleLeft] + sortedArrayNumbers[indexMiddleRight]) / 2
+  } else {
+    // return the element in the middle
+    const indexMiddleElement = Math.floor(numElements / 2) + 1
+    return sortedArrayNumbers[indexMiddleElement]
+  }
+}
+
+const mode = calculateMode(arrayGrades)
+console.log('mode: ', mode)
+
+function calculateVariance(arrayNumbers) {
+  const average = calculateAverage(arrayNumbers)
+
+  let summSquareDifferences = 0
+  arrayNumbers.forEach(value => {
+    const squareDiff = (value - average) * (value - average)
+    summSquareDifferences += squareDiff
+  })
+
+  const variance = summSquareDifferences / (arrayNumbers.length - 1)
+  return variance
+}
+
+function calculateStandardDeviation(arrayNumbers) {
+  const variance = calculateVariance(arrayNumbers)
+  return Math.sqrt(variance)
+}
+
+const standardDeviation = calculateStandardDeviation(arrayGrades)
+console.log('standardDeviation: ', standardDeviation)
