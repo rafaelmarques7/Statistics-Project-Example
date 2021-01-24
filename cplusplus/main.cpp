98e80b15f3c4429d2f94eb88b0313a5fcfec52f2
rafael <rafael@BMDIG0021.local>
2021-01-24 18:11:42 +0000

    read student grades into vector; write function to calculate average

--- cplusplus/main.cpp
+++ cplusplus/main.cpp
@@ -11,11 +11,22 @@ struct sstudent {
   float grade;
 };
 
+float calculateAverage(vector<int>& array, int numElements) {
+  int sum = 0;
+  for (int i=0; i<numElements; i++) {
+    sum += array[i];
+  }
+  float average = (float)sum / numElements;
+  return average;
+}
+
 int main () 
 {
   string line;
   string filepath = "../student-grades.txt";
   ifstream gradesFile (filepath);
+
+  vector<sstudent> studentsVector;
 
   if (gradesFile.is_open())
   {
@@ -32,19 +43,32 @@ int main ()
       while(getline(ss, token, ',')) {
         if (wordPosition == 0) {
           student.name = token;
+          wordPosition += 1;
         } else {
           token.erase(remove_if(token.begin(), token.end(), ::isspace), token.end()); // remove whitespace
           student.grade = stof(token);
           wordPosition = 0;
+
+          studentsVector.push_back(student);
         }
-        wordPosition += 1;
       }
-      
-      cout << student.name << ' ' << student.grade << '\n';
     }
     gradesFile.close();
   } else {
     cout << "Unable to open grades file";
     return 0;
   }
+
+  vector<int> studentGrades;
+
+  // print all student data in students vector
+  for (auto i = studentsVector.begin(); i != studentsVector.end(); ++i) {
+    // cout << (*i).name << " " << (*i).grade << "\n"; 
+    studentGrades.push_back((*i).grade);
+  }
+
+  int numElements = studentGrades.size();
+  float average = calculateAverage(studentGrades, numElements);
+
+  cout << "average grade: " << average << '\n';
 }
